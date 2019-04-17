2563cda2c74727ad759b799327c3e846c254ee60
German <me@g3rm4n.xyz>
2019-04-17 14:00:40 -0500

    Implementation of react router

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 180,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	parser: 'babylon',
+	useTabs: true,
+	tabWidth: 2,
+	semi: true
+};
--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './views/About';
+import Index from './views/Index';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello world del chalan.
-            <br/>
-            <label>
-              Name:
-              <input type="text" name="name" />
-            </label>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about/">About</Link>
+							</li>
+							<li>
+								<Link to="/users/">Users</Link>
+							</li>
+						</ul>
+					</nav>
+
+					<Route path="/" exact component={Index} />
+					<Route path="/about/" component={About} />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
